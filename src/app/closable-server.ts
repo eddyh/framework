a785b4c38568c83cb15884f49fd4e22ddee3893c
Edwin Westerhoud <edwinwesterhoud@voormedia.com>
2020-08-10 14:30:00 +0200

    Logging.

--- src/app/closable-server.ts
+++ src/app/closable-server.ts
@@ -9,26 +9,32 @@ export class ClosableServer extends Server {
     super()
 
     this.on("connection", (socket: Socket) => {
+      console.log("log 1")
       this.sockets.set(socket, 0)
 
       socket.on("close", () => {
+        console.log("log 2")
         this.sockets.delete(socket)
       })
     })
 
     this.on("request", (request: IncomingMessage, response: ServerResponse) => {
+      console.log("log 3")
       const socket = request.socket
       this.sockets.set(socket, +this.sockets.get(socket)! + 1)
-
+      console.log("log 4")
       if (this.closing) {
+        console.log("log 5")
         response.setHeader("Connection", "close")
       }
-
+      console.log("log 6")
       response.on("finish", () => {
+        console.log("log 7")
         const pending = +this.sockets.get(socket)! - 1
         this.sockets.set(socket, pending)
 
         if (this.closing && pending === 0) {
+          console.log("log 8")
           socket.end()
         }
       })
@@ -36,11 +42,13 @@ export class ClosableServer extends Server {
   }
 
   close(callback?: (err?: Error) => void): this {
+    console.log("log 9")
     super.close(callback)
 
     this.closing = true
 
     process.nextTick(() => {
+      console.log("log 10")
       for (const [socket, pending] of this.sockets) {
         if (pending === 0) {
           socket.end()
